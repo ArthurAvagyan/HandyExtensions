a481e1171c870818b328bb52917970c92c86d1cc
Arthur <arthur@codics.am>
2020-06-15 17:37:06 +0400

    Initial project setup

---
+++ Handy Extensions/Handy_Extensions.h
@@ -0,0 +1,18 @@
+//
+//  Handy_Extensions.h
+//  Handy Extensions
+//
+//  Created by Arthur Avagyan on 6/15/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Handy_Extensions.
+FOUNDATION_EXPORT double Handy_ExtensionsVersionNumber;
+
+//! Project version string for Handy_Extensions.
+FOUNDATION_EXPORT const unsigned char Handy_ExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Handy_Extensions/PublicHeader.h>
+
+
